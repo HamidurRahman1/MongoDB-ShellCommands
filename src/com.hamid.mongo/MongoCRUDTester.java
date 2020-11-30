e7f2bccc57e4ef2f763592644c197754222d7e70
Hamidur Rahman <Hamidur0405@gmail.com>
2020-11-30 17:49:37 -0500

    mongo client instance creation

--- src/com.hamid.mongo/MongoCRUDTester.java
+++ src/com.hamid.mongo/MongoCRUDTester.java
@@ -1,10 +1,25 @@
 package com.hamid.mongo;
 
-import com.mongodb.internal.build.MongoDriverVersion;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+import java.util.Arrays;
 
 public class MongoCRUDTester
 {
-    public static void main(String[] args) {
-        System.out.println(MongoDriverVersion.VERSION);
+    public static void main(String[] args)
+    {
+        System.out.println(mongoClientUsingClientSettings().getDatabase("demo").getName());
+    }
+
+    protected static MongoClient mongoClientUsingClientSettings()
+    {
+        return MongoClients.create(MongoClientSettings
+                .builder()
+                .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress("hostOne", 27017))))
+                .build());
+
     }
 }
