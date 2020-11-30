764b77ff4fb700977cc7e187a7503a3c26fc9a32
Hamidur Rahman <Hamidur0405@gmail.com>
2020-11-30 17:30:00 -0500

    mongo

---
+++ src/com.hamid.mongo/MongoCRUDTester.java
@@ -0,0 +1,10 @@
+package com.hamid.mongo;
+
+import com.mongodb.internal.build.MongoDriverVersion;
+
+public class MongoCRUDTester
+{
+    public static void main(String[] args) {
+        System.out.println(MongoDriverVersion.VERSION);
+    }
+}
